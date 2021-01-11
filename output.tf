efb8b18e96989c9a2ba07823d0542fea3b31c0c3
jose <jose.hernandez@isthari.com>
2021-01-11 09:02:19 +0100

    EDDIE-248 configure cloud region

--- output.tf
+++ output.tf
@@ -7,23 +7,46 @@ locals {
     dnsNs3 = aws_route53_zone.default.name_servers.2
     dnsNs4 = aws_route53_zone.default.name_servers.3
 
-    # Certificate validation info
-    acm_validation_name  = aws_acm_certificate.wildcard.domain_validation_options.0.resource_record_name
-    acm_validation_value = aws_acm_certificate.wildcard.domain_validation_options.0.resource_record_value
+    # account 
+    externalAccountId = data.aws_caller_identity.current.account_id
+    region            = var.region
+
+    # security
+    keyPair          = var.keyPair
 
     # Network
     vpcId            = aws_vpc.default.id
-    publicSubnet1id  = aws_subnet.public-1.id
-    publicSubnet2id  = aws_subnet.public-2.id
-    privateSubnet1id = aws_subnet.private-1.id
+    publicSubnet1Id  = aws_subnet.public-1.id
+    publicSubnet2Id  = aws_subnet.public-2.id
+    privateSubnet1Id = aws_subnet.private-1.id
+    privateSubnet2Id = aws_subnet.private-2.id
 
     # ALB
-    albArn = aws_alb_listener.default.arn
+    albEndpoint    = aws_lb.default.dns_name
+    albListenerArn = aws_alb_listener.default.arn
+
+    # bucket
+    bucketName = var.bucket
+
+    # instance
+    instanceSecurityGroup = aws_security_group.instance.id
+    instanceProfile = aws_iam_instance_profile.instance.name
   }
 }
 
 resource "local_file" "configuration" {
   content  = jsonencode(local.configuration)
   filename = "${path.module}/dist/${var.cloudRegionId}.json"
+
+provisioner "local-exec" {
+    command = <<EOT
+        curl -X POST http://localhost/api/region-manager-aws/register/postupsert \
+                -H 'Content-Type: application/json' \
+                -H 'cloudRegionId: ${var.cloudRegionId}' \
+                -H 'shortId: ${var.shortId}' \
+                -d @${path.module}/dist/${var.cloudRegionId}.json 
+EOT
+  }
+
 }
 
